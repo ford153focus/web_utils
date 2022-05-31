824289f9ff9c1e2025a7d404c9f22b371b839955
Joe Ford <ford153focus@gmail.com>
2022-05-31 08:59:57 +0300

    fix lines

--- libbrowser.py
+++ libbrowser.py
@@ -41,6 +41,7 @@ elif sys.platform == 'linux':
 
 launch_options['args'] = launch_arguments
 
+
 def get_chromium_user_data_dir(user_name):
     return os.path.join(
         os.path.expanduser("~"),
@@ -50,6 +51,7 @@ def get_chromium_user_data_dir(user_name):
         user_name
     )
 
-async def click_if_exists(page:pyppeteer.page.Page, selector:str):
+
+async def click_if_exists(page: pyppeteer.page.Page, selector: str):
     if await page.querySelector(selector) is None: return
     await page.click(selector)
